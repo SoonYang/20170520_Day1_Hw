fc6ce68e78e79460dfd0aefa79cf56833085ad74
CiC <k17263636@gmail.com>
2017-05-24 23:29:55 +0800

    Create Test project

---
+++ 20170520_Homework/20170520_HomeworkTests/OrderUtilityTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _20170520_Homework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20170520_Homework.Tests
+{
+    [TestClass()]
+    public class OrderUtilityTests
+    {
+        [TestMethod()]
+        public void GetSumTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
