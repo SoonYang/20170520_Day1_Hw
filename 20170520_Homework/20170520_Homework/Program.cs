02fb9757eefc23fb5896b95970431f2cebf65698
CiC <k17263636@gmail.com>
2017-05-24 23:08:03 +0800

    Create solution with a console app

---
+++ 20170520_Homework/20170520_Homework/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20170520_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
