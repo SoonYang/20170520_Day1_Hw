f068ec1df15a829ab3e58946f6ee1075a5fe86dd
CiC <k17263636@gmail.com>
2017-05-24 23:25:57 +0800

    create OrderUtility and it's interface

---
+++ 20170520_Homework/20170520_Homework/IOrderUtility.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace _20170520_Homework
+{
+    /// <summary>
+    /// 訂單工具
+    /// </summary>
+    public interface IOrderUtility
+    {
+        /// <summary>
+        /// 取得指定欄位總和
+        /// </summary>
+        /// <param name="colName">欄位名稱</param>
+        /// <param name="groupByNum">每幾筆資料統計為一組</param>
+        /// <returns></returns>
+        IEnumerable<int> GetSum(string colName, int groupByNum);
+    }
+}
---
+++ 20170520_Homework/20170520_Homework/OrderUtility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20170520_Homework
+{
+    public class OrderUtility : IOrderUtility
+    {
+        public IEnumerable<int> GetSum(string colName, int groupByNum)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
